5281ddd03fc13100ba36f1996a5811231fd635c1
Dinesh <gnana.dinesh@hpe.com>
2019-06-28 09:37:51 +0530

    success

---
+++ src/org/cts/test/moto.java
@@ -0,0 +1,22 @@
+package org.cts.test;
+
+import java.time.chrono.AbstractChronology;
+
+public class moto extends vivo {
+     
+	public void compId()
+	{
+		System.out.println("4321");
+	}
+	public void compname()
+	{
+		System.out.println("cts");
+	}
+	public static void main(String[] args) {
+		moto a = new moto();
+		a.compId();
+		a.empId();
+		a.empname();
+		a.compname();
+	}
+}
---
+++ src/org/cts/test/vivo.java
@@ -0,0 +1,14 @@
+package org.cts.test;
+
+public class vivo {
+	public void empId()
+	{
+		System.out.println("1234");
+		
+	}
+	public void empname()
+	{
+		System.out.println("farith");
+	}
+
+}
